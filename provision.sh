76d2bc756fcb9762a6d473719165beee8616519e
SHIRA <mshr0520@icloud.com>
2019-11-19 05:01:01 -0800

    仮想環境とローカル環境のファイル共有

--- provision.sh
+++ provision.sh
@@ -1,9 +1,17 @@
 # インストール済みのパッケージをアップデート
 sudo yum -y update
 
+
+
 # よく使うコマンドをインストール
 sudo yum -y install wget unzip yum-utils mlocate
 sudo updatedb
+# Laravelのファイルで解凍コマンド必要な必要なためインストール
+sudo yum -y install zip unzip
+#php7.1とphp moduleのインストール
+sudo yum install -y --enablerepo=remi,remi-php71 php-common php-pdo php-cli php-devel php-mysqlnd php-mbstring php-gd php-intl php-xml php-bcmat
+
+
 
 # epel, remi リポジトリを追加して有効化
 sudo yum -y install epel-release
