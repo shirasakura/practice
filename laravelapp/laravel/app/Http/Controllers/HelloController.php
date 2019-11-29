c7802a87f74dc5a932de1c6aef7cf0a0814125ee
SHIRA <mshr0520@icloud.com>
2019-11-28 16:58:05 -0800

    ペジネーション DBクラスとsimplePaginate
    ・５レコードのみの表示
    ・Next/Prevの表示
    ・hrのリンクを押したらorderByでascにする

--- laravelapp/laravel/app/Http/Controllers/HelloController.php
+++ laravelapp/laravel/app/Http/Controllers/HelloController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use App\Person;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
-        return view('hello.index', ['items' => $items]);
+        $sort = $request->sort;
+        $items = DB::table('people')->orderBy($sort,'asc')->simplePaginate(5);
+        $param = ['items'=>$items, 'sort'=>$sort];
+        return view('hello.index', $param);
     }
     public function post(Request $request)
     {
--- laravelapp/laravel/resources/views/hello/index.blade.php
+++ laravelapp/laravel/resources/views/hello/index.blade.php
@@ -2,6 +2,14 @@
 {{-- 継承レイアウト/子レイアウト（テンプレート） .. /hello --}}
 
 @extends('layouts.helloapp')
+<style>
+  .pagination{font-size: 10pt;}
+  .pagination li{display: inline-block;}
+  tr th a:link{color: white;}
+  tr th a:visited{color: white;}
+  tr th a:hover{color: white;}
+  tr th a:active{color: white;}
+</style>
 
 @section('title', 'Index')
 
@@ -12,7 +20,11 @@
 
 @section('content')
 <table>
-  <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+  <tr>
+    <th><a href="/hello?sort=name">Name</a></th>
+    <th><a href="/hello?sort=mail">Mail</a></th>
+    <th><a href="/hello?sort=age">Age</a></th>
+  </tr>
   @foreach ($items as $item)
       <tr>
         <td>{{$item->name}}</td>
@@ -21,6 +33,7 @@
       </tr>
   @endforeach
 </table>
+{{$items->appends(['sort'=>$sort])->links()}}
 @endsection
 @section('footer')
     copyright 2017 kokoa.
